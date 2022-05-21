95f3e4e6662e75243933bc9a5ad2957901bb7395
Chenwei <Chenweigenius@gmail.com>
2022-05-21 13:40:33 +0800

    添加空间分析的按钮

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import VueRouter from "vue-router";
 import Coverage from "../components/coverage.vue";
 import Assets from "../components/assets.vue";
 import Upload from "../components/upload.vue";
+import Analysis from "../components/analysis.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/assets", component: Assets },
   { path: "/coverage", component: Coverage },
-  { path: "/upload", component: Upload }
+  { path: "/upload", component: Upload },
+  { path: "/analysis", component: Analysis }
 ];
 
 const router = new VueRouter({
