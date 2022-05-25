06e15cdfd8233e1055fd7b596515a46491c76c4c
OrkWard <71199274+OrkWard@users.noreply.github.com>
2022-05-25 19:15:59 +0800

    修复了代理目标的bug

--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api/": {
-        target: "http://127.0.0.1:8000/api",
+        target: "http://10.185.22.111/api",
         changeOrigin: true,
         pathRewrite: {
           "^/api": ""
